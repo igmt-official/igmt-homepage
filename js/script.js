9d9afcd05b9529f6be78a9fa1cda4d68396c5db1
Ivan Gregor Tabalno <96023410+igmt-official@users.noreply.github.com>
2022-04-13 10:48:55 +0800

    Added scroll behavior.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ const bodyEl = document.querySelector(".body");
 btnNavEl.addEventListener("click", function () {
   bodyEl.classList.toggle("nav-open");
 });
+
+// Scroll behavior for all browser
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
